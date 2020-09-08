f9f5f73a547eaa900cc66cc65b37933a6fad7e28
Nur-aly <chiketaevn@gmail.com>
2020-09-08 18:04:41 +0600

    local settings

---
+++ myblog/local_settings.py
@@ -0,0 +1,1 @@
+SECRET_KEY = '=!4z7md6av9$!=s$vgd#d6y1t+$%mo%p8&t*lr=1=b=a3mj2^c'
--- myblog/settings.py
+++ myblog/settings.py
@@ -110,10 +110,11 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
 )
-STATIC_URL = '/static/'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
