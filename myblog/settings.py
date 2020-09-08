dec4817f52e48f351683ef8a039391572d0f7617
Nur-aly <chiketaevn@gmail.com>
2020-09-08 18:18:56 +0600

    allowed hosts

--- myblog/settings.py
+++ myblog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY =local_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['salty-harbor-18048.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
